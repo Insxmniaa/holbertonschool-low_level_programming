309cc45103d0ca9079daebd3dbc5ede88cc45209
Insxmniaa <jeanne.melendez@icloud.com>
2023-10-16 13:29:51 -0400

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,6 @@
-#!/bin/bash
 #include <stdio.h>
-
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+int main(vioid)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
