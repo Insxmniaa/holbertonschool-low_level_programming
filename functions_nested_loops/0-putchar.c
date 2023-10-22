362a889581635b9644bd7c812a6fa3126dc6760d
Insxmniaa <jeanne.melendez@icloud.com>
2023-10-22 19:20:43 -0400

    ugh

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,23 +1,22 @@
-#include "main.h"
 #include <stdio.h>
+#include "holberton.h"
 
 /**
-*
-* main - Prints "_putchar" followed by a new line.
-*
-* Return: Always 0.
-*/
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-        putchar('_');
-        putchar('p');
-        putchar('u');
-        putchar('t');
-        putchar('c');
-        putchar('h');
-        putchar('a');
-        putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h’);
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-        return (0);
+	return (0);
 }
-
