ee0e70214e9fc0659ab622facdb7e0f8b12eec93
Insxmniaa <jeanne.melendez@icloud.com>
2023-10-23 11:17:14 -0400

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int_strlen - writes a function that returns the length of a string.
+ * @str: the string to get the length of
+ *
+ * Return: the length of the @str.
+ */
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+	
+	while (*str++)
+		length++;
+
+	return (length);
+}
