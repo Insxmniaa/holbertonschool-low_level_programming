6afd734b898a8dd74cc41924efc8dd43e64c99c0
Insxmniaa <jeanne.melendez@icloud.com>
2023-10-22 22:47:56 -0400

    last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_last_digit - prints the last digit of a number
  *
@@ -19,7 +20,7 @@ int print_last_digit(int n)
 	if (a < 0)
 		a = -a;
 
-	_putchar(a + '0');
+	putchar(a + '0');
 
 	return (a);
 }
