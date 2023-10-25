6eb073bd769737c277e32774e1e28be1b9c0d05a
Insxmniaa <jeanne.melendez@icloud.com>
2023-10-25 10:19:47 -0400

    mistakes need to be fixed

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,15 +3,15 @@
 
 /**
  * print_rev - Prints a string in reverse.
- * @s: The string to be printed.
+ * @s: the string to be printed.
  */
 void print_rev(char *s)
 {
 	int len = 0, index;
-
-	while (s[index++])
+	
+	while (s[index++})
 		len++;
-
+	
 	for (index = len - 1; index >= 0; index--)
 		putchar(s[index]);
 
