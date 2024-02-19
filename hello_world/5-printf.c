86558fc71e9e23672fd077064f38ac26d2deddc4
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-19 16:32:39 -0400

    print

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Prints "with proper grammer, but the outcome is a piece of art"
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of are,\n");
+	return (0);
+}
+
