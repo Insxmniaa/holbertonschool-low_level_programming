5ab3135dcc1fcab4e38f86282e1fa45105c21110
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-19 16:24:19 -0400

    fixing

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints "Programming is like building a multilingual puzzle, followed by a new line"
  *
@@ -8,7 +7,6 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 
 }
