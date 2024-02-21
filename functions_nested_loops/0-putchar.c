f2a7c9480f33861044c17bf283dd3d90ddd4db8a
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-21 14:51:57 -0400

    ?

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
-#include <main.h>
+#include "main.h"
+
 /**
- * main - prints "_putchar"
- * Return: Always 0.
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
