2b841e70713c1a70bd9238896780fe3e8a50d4ed
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-20 23:03:59 -0400

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations fo single-digit numbers.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
