7fd303fe739840b79c309b93c26d79a8ea573262
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-26 22:39:45 -0400

    fi

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - function with 1 argument
+ * @s: 1st argument char type
+ * Return: na
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	};
+	_putchar('\n');
+}
