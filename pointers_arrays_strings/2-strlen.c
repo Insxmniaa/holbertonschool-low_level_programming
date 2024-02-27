f67beb25a794d246307be3562b6e75aec9dbfb71
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-26 22:30:14 -0400

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function with 1 argument
+ * @s: char type pointer
+ * Return: string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
