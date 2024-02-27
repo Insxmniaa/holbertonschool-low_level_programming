d2da8ed4ff7a546531b1d1f98ba08a2238ab0d15
Insxmniaa <jeanne.melendez@icloud.com>
2024-02-26 22:48:31 -0400

    ?

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,22 +7,27 @@
  */
 void puts_half(char *str)
 {
-	int i, len;
+	int len, n, i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	len = i;
-	if (len % 2 != 0)
-		len = (len + 1) / 2;
-	else
-		len /= 2;
-	while (len < i)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
 		len++;
-		putchar(*(str + len - 1));
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			putchar(str[n + 1]);
+		}
 	}
 	putchar('\n');
 }
